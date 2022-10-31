65480e2e9a22f93b52082ea6c7c45fae92c7bc9f
KTullock <kailatullock@aol.com>
2022-10-31 18:51:41 -0400

    add arrays in JavaScript

--- script.js
+++ script.js
@@ -2,20 +2,51 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  
   var userInput = prompt("How many characters do you want in your password?")
+  var length = parseInt(userInput)
 
-  var passwordLength = parseInt(userInput)
-
-  if (isNaN(passwordLength)) {
+    if (isNaN(length)) {
     alert("Please enter a number.")
-    //MAY NEED TO ADD A "return"
+    return
   }
 
-  if (passwordLength < 8 || passwordLength > 128) {
+  if (length < 8 || length > 128) {
     alert("Please enter a number between 8 and 128.")
-    //MAY NEED TO ADD A "return"
+    return
+  }
+
+  var needLowercase = prompt("Do you need lowercase letters? Enter 'true' or 'false'.");
+  var needUppercase = prompt("Do you need uppercase letters? Enter 'true' or 'false'.");
+  var needNumeric = prompt("Do you need numbers? Enter 'true' or 'false'.");
+  var needSpecial = prompt("Do you need special characters? Enter 'true' or 'false'.");
+
+  var outputLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+  "n", "o","p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var outputUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+  "N", "O","P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var outputNumeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var outputSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_"]
+
+  var charContainer = []
+
+  if (needLowercase === true) {
+    charContainer.push(outputLowercase)
+  }
+
+  if (needUppercase === true) {
+    charContainer.push(outputUppercase)
+  }
+
+  if (needNumeric === true) {
+    charContainer.push(outputNumeric)
+  }
+
+  if (needSpecial === true) {
+    charContainer.push(outputSpecial)
   }
 }
+
 
 // Write password to the #password input
 function writePassword() {
